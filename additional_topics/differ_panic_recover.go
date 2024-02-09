4f696b0b52eee35e66b0a97936470ec3472e035c
mike-nzioki <mikenzioki@Michaels-MacBook-Air-2.local>
2024-02-09 10:14:42 +0300

    learned go defer, panic & recover

--- additional_topics/differ_panic_recover.go
+++ additional_topics/differ_panic_recover.go
@@ -26,6 +26,46 @@ func main() {
 
 
 	// # panic
-	// - 
+	// - we use panic to immediately terminate execution of a program
+	fmt.Println("Help! something is wrong")
+	panic( "Ending the Program")
+	fmt.Println("Waiting to be executed")
 
+	division(4, 2)
+	division(8, 0)
+	division(2, 8)
+
+
+
+	// # RECOVER IN Go
+	//  - the panic statement terminates the program but it is important for program to complete its execution
+	//  - we use recover statement to to hadle panic in go 
+	//  - recover statement prevents termination of the program and recovers the program for panic
+
+
+}
+
+
+func division(num1, num2 int) {
+
+	// execute handlePanic even befor panic occurs
+	// defer handlePanic()
+	// if num2 is 0, program is terminated due to panic
+
+	if num2 == 0 {
+	  panic("Cannot divide a number by zero")
+	} else {	
+	  result := num1 / num2
+	  fmt.Println("Result: ", result)
+	}
+	
+}
+
+
+// recover
+func handlePanic() {
+	a := recover()
+	if a != nil {
+		fmt.Println("RECOVER", a)
+	}
 }
